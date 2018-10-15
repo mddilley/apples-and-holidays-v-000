0950d29c6768a056d7edbcfdd75c82b6fbbe4a21
Mike Dilley <mike.dilley@gmail.com>
2018-10-15 14:28:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_season_hash|
     puts "#{season}:".capitalize!
     holiday_season_hash.each do |holiday, holiday_array|
+      (holiday.split("_").each {|word| word.capitalize!}).join(" ")
       puts "  #{holiday}: ".capitalize!
         holiday_array.each do |holiday|
           holiday
